f3eeb8d3989b7dc15b120aff93559196d56e9f27
Sudharsanram <sudharsanram.m@gmail.com>
2024-05-11 19:30:32 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-# 19AI308-Object-Oriented-Programming-using-CSharp-Exp-10-File-Manipulation
-Develop a C# program to get the values from the user using structure and store it in a file in a specific path using file stream concept
-# AIM:
-To develop a C# program using file streams.
-
-# PROGRAM:
-```
-using System;
+﻿using System;
 using System.IO;
 
 struct UserData
@@ -48,10 +41,3 @@ class Program
         }
     }
 }
-```
-# OUTPUT:
-![alt text](image.png)
-
-![alt text](image-1.png)
-# RESULT:
-Thus the C# program to get the values from the user using structure and store it in a file in a specific path using file stream concept executed successfully.
